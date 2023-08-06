93c184107df039d9b327cf1c35fa1aa1d5ade947
lgndev <logan.sawyer.dev@gmail.com>
2023-08-06 12:38:45 -0400

    ready to start

--- react-interview-PE/src/App.tsx
+++ react-interview-PE/src/App.tsx
@@ -1,9 +1,36 @@
 import { useState } from "react";
+import { login } from "./util/util";
+
+// Guidelines:
+// * You have an incomplete login form
+// * You are not allowed to add any additional HTML elements
+// * You are not allowed to use refs
+
+// Tasks:
+// * The login button should trigger the login() action imported above and pass required data to it
+// * Disable the Login button if email is blank OR if password is under 6 letters
+// * Disable the Login button while login action is being performed
+// * Show an error message from the login() if login fails. The error should be cleared every time user re-attaches to log in
+// * Show an alert box (naitive JS) if login succeeds. Investigate the login function to find out how to log in successfully
 
 function App() {
   return (
     <>
-      <div>vite</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div>
+          <label htmlFor="email">email</label>
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">password</label>
+          <input type="password" id="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 }
---
+++ react-interview-PE/src/util/util.ts
@@ -0,0 +1,17 @@
+interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export const login = async ({ email, password }: LoginCredentials) => {
+  const delay = (0.07 + Math.random() * 2) * 1000;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (password === "password123" && !!email) {
+        resolve(null);
+      } else {
+        reject(new Error("Invlaid email or password"));
+      }
+    }, delay);
+  });
+};
