499584b23f93e12181025da111cf9079372d61d4
lgndev <logan.sawyer.dev@gmail.com>
2023-08-28 09:11:50 -0400

    resolved

---
+++ counter-tailwind/src/App.jsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+
+// Coding challenge colors:
+// Yellow click button background: #ffbb48
+// Black bkgd #000000
+// Error text color: #f61c25
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [error, setError] = useState("");
+
+  //  decrement
+  const handleDec = () => {
+    setError("");
+    // no - numbers
+    if (counter > 0) {
+      setCounter((prev) => prev - 1);
+    } else {
+      setError("no negative number");
+    }
+  };
+
+  // increment
+  const handleIncremnet = () => {
+    if (counter < 10) {
+      setCounter((prev) => prev + 1);
+      setError("");
+    } else {
+      setError("can only go up to 10");
+    }
+  };
+
+  // reset
+  const handleReset = () => {
+    setCounter(0);
+    setError("");
+  };
+
+  return (
+    <>
+      <div className="h-screen w-screen bg-black flex justify-center items-center">
+        <div className="w-[400px] bg-white p-[25px]">
+          <div className="mb-[20px] flex justify-center">
+            <button
+              className="uppercase bg-transparent border-0 text-[orange] text-[18px] cursor-pointer"
+              onClick={() => {
+                handleReset();
+              }}
+            >
+              reset
+            </button>
+          </div>
+          <div className="flex justify-evenly items-center mb-[20px]">
+            <button
+              onClick={() => {
+                handleDec();
+              }}
+              className="uppercase bg-[#ffbb48] border-0 text-black h-[40px] w-[40px] rounded-[50%] text-[30px] curspor-pointer flex justify-center items-center"
+            >
+              -
+            </button>
+
+            <input
+              type="number"
+              value={counter}
+              className="border-2 border-black"
+              onChange={(e) => {
+                setCounter(e.target.value);
+              }}
+              onBlur={(e) => {
+                if (Number(e.target.value) > 10) {
+                  setCounter(Number(10));
+                  setError("can only go up to 10");
+                } else if (Number(e.target.value) < 0) {
+                  setCounter(Number(0));
+                  setError("no negative number");
+                } else {
+                  setError("");
+                  setCounter(Number(e.target.value));
+                }
+              }}
+            />
+
+            <button
+              onClick={() => {
+                handleIncremnet();
+              }}
+              className="uppercase bg-[#ffbb48] border-0 text-black h-[40px] w-[40px] rounded-[50%] text-[30px] curspor-pointer flex justify-center items-center"
+            >
+              +
+            </button>
+          </div>
+          <p className="text-[#f61c25] text-center text-[18px] h-[18px]">
+            {error}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
+
+// next steps
+// - this week next week live coding challenges
+// - last one next tuesday 29th
+
+// - pick candiate to vp of engineering
+// - will be muiltiple
+// - mid to late next week should hear back
+// - sit down meet and greet
+// - no tech challenge
+// - qiuestions about apst experience
+// -
+
+// - asher asked me about why i used inline styles
+// - I exaplained that this was for the context of the exercise and time contraints
+// - I could have quickyl showed him that i knew how to write classes and maybe create and import one into the exampel
+
+// - I got caught up an type casting
+// - didnt realize vlaue enteters into input was string
+// - I could have explained why I was getting a weired state value
+
+// - use developer tools and degbugger more!
+// - I could have seen that value of state in chrome dev tools
